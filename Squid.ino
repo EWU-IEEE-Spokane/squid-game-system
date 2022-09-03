7a956d75a213977e7c50efb266603f174fb20281
EWU-IEEE <76026201+EWU-IEEE@users.noreply.github.com>
2022-09-03 13:05:23 -0700

    Add files via upload

--- Squid.ino
+++ Squid.ino
@@ -8,34 +8,44 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(redLed, OUTPUT);
   pinMode(greenLed, OUTPUT);
+  digitalWrite(redLed, LOW);
+  digitalWrite(greenLed, HIGH);
   Serial.begin(9600);
 }
 
 void loop() {
   //Generate random delay, switch between states after delay
-  int delayTime = random(2);
-  digitalWrite(redLed, HIGH);
-  digitalWrite(greenLed, LOW);
-  delay(1000*delayTime);
-  //redLight(delayTime);
-  delayTime = random(2);
-  digitalWrite(redLed, LOW);
-  digitalWrite(greenLed, HIGH);
-  delay(1000*delayTime);
-  Serial.println(millis());
-
-  //greenLight(delayTime);
-  
+  int delayTime = random(1000, 3000);
+  redLight(delayTime);
+  delayTime = random(1000, 3000);
+  greenLight(delayTime);
 }
 
 //void switchColor (){
 //  
-//}
 //
-//void redLight(delayTime){
-//  
-//}
 //
-//void greenLight(delayTime){
-//  
-//}
+
+
+void redLight(delayTime){
+  toggle_led();
+  int start = millis();
+  while (millis() < (start + delayTime){
+    //if button is pressed and (digitalRead(redLed), you lose!
+  }
+}
+
+void greenLight(delayTime){
+  toggle_led();
+  int start = millis();
+  while (millis() < (start + delayTime){
+    //if button is pressed, advance stepper
+  }  
+}
+
+bool toggle_led(void *) {
+  //rotate eagle head
+  digitalWrite(redLed, !digitalRead(redLed)); // toggle the LED
+  digitalWrite(greenLed, !digitalRead(greenLed)); // toggle the LED
+  return true; // repeat? true
+}
